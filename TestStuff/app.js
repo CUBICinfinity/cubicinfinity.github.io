3ffa40400ac79d5d735754c30036df45ca008a43
∞³ Jim Greene <jimkgreene@gmail.com>
2022-03-25 12:54:48 -0600

    match elementor version
    "/wp-content/scripts/" is "/TestStuff/"

--- TestStuff/app.js
+++ TestStuff/app.js
@@ -110,20 +110,20 @@ function sanitizeHTML(strings) {
  */
 function initMap() {
   // Create the map.
-  const map = new google.maps.Map(document.getElementById('map'), {
+  const map = new google.maps.Map(document.getElementById('store_map'), {
     zoom: 7,
     center: {lat: 52.632469, lng: -1.689423},
     styles: mapStyle,
   });
 
   // Load the stores GeoJSON onto the map.
-  map.data.loadGeoJson('stores.json', {idPropertyName: 'storeid'});
+  map.data.loadGeoJson('/TestStuff/map_locations.json', {idPropertyName: 'storeid'});
 
   // Define the custom marker icons, using the store's "category".
   map.data.setStyle((feature) => {
     return {
       icon: {
-        url: `img/icon_${feature.getProperty('category')}.png`,
+        url: `/TestStuff/map_img/icon_${feature.getProperty('category')}.png`,
         scaledSize: new google.maps.Size(64, 64),
       },
     };
@@ -141,7 +141,7 @@ function initMap() {
     const phone = event.feature.getProperty('phone');
     const position = event.feature.getGeometry().get();
     const content = sanitizeHTML`
-      <img style="float:left; width:200px; margin-top:30px" src="img/logo_${category}.png">
+      <img style="float:left; width:200px; margin-top:30px" src="/TestStuff/map_img/logo_${category}.png">
       <div style="margin-left:220px; margin-bottom:20px;">
         <h2>${name}</h2><p>${description}</p>
         <p><b>Open:</b> ${hours}<br/><b>Phone:</b> ${phone}</p>
